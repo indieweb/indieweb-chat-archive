c2c44486c1e97736d6097970ff2aa806dfc719cb
Aaron Parecki <aaron@parecki.com>
2021-07-01 14:25:39 +0000

    logs as of Thu Jul  1 14:25:39 UTC 2021

--- git-sync.sh
+++ git-sync.sh
@@ -3,7 +3,6 @@
 cd /web/sites/chat.indieweb.org/data
 /usr/bin/git add -A
 /usr/bin/git commit -m "logs as of `date`"
-/usr/bin/git pull origin master
-/usr/bin/git push origin master
-/usr/bin/git push aaronpk master
+/usr/bin/git pull origin main --depth 10
+/usr/bin/git push origin main
 
